329398fa99f2bad47b96c60ec96ab699292babe0
southamericanbear <dittler.a@gmail.com>
2024-03-31 22:43:10 -0300

    added secret

--- auth.config.ts
+++ auth.config.ts
@@ -25,4 +25,5 @@ export const authConfig = {
     secret: process.env.JWT_SECRET,
   } as any,
   providers: [], // Add providers with an empty array for now
+  secret: process.env.AUTH_SECRET,
 } satisfies NextAuthConfig;
