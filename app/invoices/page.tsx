fcfeaa8f19c90600eda46c448e6ee9e4f2eeeed1
southamericanbear <dittler.a@gmail.com>
2024-04-02 12:41:28 -0300

    create invoicepage

--- app/invoices/page.tsx
+++ app/invoices/page.tsx
@@ -8,6 +8,10 @@ export default function InvoicesPage() {
   return (
     <SidebarMenuLayout>
       <h1>Invoices</h1>
+
+      <button draggable="true" className="border-gray-500 border-solid">
+        hola
+      </button>
     </SidebarMenuLayout>
   );
 }
