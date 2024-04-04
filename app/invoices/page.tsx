aea1d5a88e363f45b3717deeb29eaa8adf4e181e
southamericanbear <dittler.a@gmail.com>
2024-04-04 00:34:58 -0300

    invoice page v1 finish

--- app/invoices/page.tsx
+++ app/invoices/page.tsx
@@ -1,7 +1,9 @@
+import moment from "moment";
 import { InvoicesTable } from "@/components/invoices-table";
 import { SidebarMenuLayout } from "@/components/layouts";
 import { UploadInvoice } from "@/components/upload-invoice";
 import { getInvoiceDetails } from "@/lib/data/invoices";
+import { CurrentMonthInvoicesInfo } from "@/components/current-month-invoices-info";
 
 export const metadata = {
   title: "Invoices",
@@ -10,14 +12,27 @@ export const metadata = {
 export default async function InvoicesPage() {
   const data = await getInvoiceDetails();
 
+  function handleGetCurrentMonthInvoices() {
+    const currentMonth = moment().format("MMMM").toLowerCase();
+    const currentYear = moment().format("YYYY");
+
+    const currentMonthInvoices = data.find(
+      (invoice) =>
+        invoice.month === currentMonth && invoice.year === currentYear
+    );
+
+    return currentMonthInvoices ?? null;
+  }
+
   return (
     <SidebarMenuLayout>
-      <h1>Invoices</h1>
       <div className="grid grid-cols-1 md:grid-cols-2 gap-4 max-w-screen-lg">
         <div className="h-fit">
           <UploadInvoice />
         </div>
-        <div className="bg-red-500 h-32"></div>
+        <div className="h-full">
+          <CurrentMonthInvoicesInfo invoice={handleGetCurrentMonthInvoices()} />
+        </div>
         <div className="md:col-span-2">
           <InvoicesTable invoices={data} />
         </div>
---
+++ components/current-month-invoices-info/index.tsx
@@ -0,0 +1,48 @@
+import React from "react";
+import ItemsCardLayout from "../layouts/items-card-layout";
+import { InvoicesInsights } from "@/types";
+import { CardContent, CardHeader, CardTitle, Progress } from "../ui";
+
+interface CurrentMonthInvoicesInfoProps {
+  invoice: InvoicesInsights | null;
+}
+
+const CURRENT_LIMIT = 458000;
+
+export const CurrentMonthInvoicesInfo = ({
+  invoice,
+}: CurrentMonthInvoicesInfoProps) => {
+  return (
+    <ItemsCardLayout className={`max-w-lg h-full ${!invoice && "flex"}`}>
+      {invoice ? (
+        <>
+          <CardHeader>
+            <CardTitle>Current Month Info</CardTitle>
+          </CardHeader>
+          <CardContent>
+            <span className="text-lg font-semibold">
+              Period: {invoice.month} / {invoice.year}
+            </span>
+            <hr className="my-4 border-t border-muted-foreground" />
+            <div className="flex flex-col gap-3">
+              <span className="text-sm text-muted-foreground">
+                ${invoice.value} / ${CURRENT_LIMIT}
+              </span>
+              <Progress
+                value={(invoice.value / CURRENT_LIMIT) * 100}
+                indicatorClasses="bg-gray-700"
+              />
+            </div>
+          </CardContent>
+        </>
+      ) : (
+        <div className="flex justify-center items-center">
+          <p className="text-sm text-muted-foreground p-4">
+            No invoices found for the current month. Please upload invoices to
+            view insights.
+          </p>
+        </div>
+      )}
+    </ItemsCardLayout>
+  );
+};
